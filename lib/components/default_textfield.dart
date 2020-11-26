e17192b2603fecef1449d3aa519b79796e387c64
duongkibo <kibzz0zzz@gmail.com>
2020-11-26 14:20:42 +0700

    update default textfiedl

--- lib/components/default_textfield.dart
+++ lib/components/default_textfield.dart
@@ -2,14 +2,16 @@ import 'package:flutter/material.dart';
 
 class DefaultTextfield extends StatelessWidget
 {
-  DefaultTextfield({Key key, this.function, this.textLabel, this.textHint}):super(key: key);
+  DefaultTextfield({Key key, this.function, this.textLabel, this.textHint, this.isPassword}):super(key: key);
   final Function function;
   final String textLabel;
   final String textHint;
+  final bool isPassword;
   @override
   Widget build(BuildContext context) {
     return  TextFormField(
       keyboardType: TextInputType.text,
+      obscureText: isPassword,
       autocorrect: false,
       onSaved: function,
       onChanged: function,
